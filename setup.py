185607ebf04454a4320c87b826db4f6c5ef9e5ec
Paul Maigutyak <pmaigutyak@ettera.com>
2017-01-30 18:33:28 +0200

    Rename package name.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 
 setup(
-    name='turbosms',
+    name='django-turbosms',
     version='1.0.0',
     description='Turbosms.ua django integration app',
     long_description=open('README.md').read(),
