68e9702184c1a75b0fd9c8c6531a007441d32c0e
Paul Maigutyak <pmaigutyak@ettera.com>
2017-03-21 09:52:26 +0200

    Update setup files.

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
+
+from turbosms import __version__
+
+
+with open('requirements.txt') as f:
+    requires = f.read().splitlines()
+
+
+url = 'https://github.com/pmaigutyak/mp-turbosms'
 
 
 setup(
     name='django-turbosms',
-    version='1.1',
+    version=__version__,
     description='Turbosms.ua django integration app',
     long_description=open('README.md').read(),
     author='Paul Maigutyak',
     author_email='pmaigutyak@gmail.com',
-    url='https://github.com/pmaigutyak/mp-turbosms',
-    download_url='https://github.com/pmaigutyak/mp-turbosms/archive/1.1.tar.gz',
-    packages=['turbosms'],
-    license='MIT'
+    url=url,
+    download_url='%s/archive/%s.tar.gz' % (url, __version__),
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    install_requires=requires
 )
