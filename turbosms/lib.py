bc55308f6437699f764a5b69e43a7c47fd5493a1
Paul Maigutyak <pmaigutyak@ettera.com>
2017-05-30 11:40:47 +0300

    Add site config support for sms recipients.

--- turbosms/lib.py
+++ turbosms/lib.py
@@ -1,14 +1,27 @@
 
+from django.apps import apps
 from django.template.loader import render_to_string
 
 from turbosms.settings import IS_SMS_ENABLED, SMS_RECIPIENTS
 from turbosms.models import SMS
 
 
+def get_recipients():
+
+    if apps.is_installed('site_config'):
+
+        from site_config import config
+
+        if hasattr(config, 'SMS_RECIPIENTS'):
+            return config.SMS_RECIPIENTS
+
+    return SMS_RECIPIENTS
+
+
 def send_sms(message):
 
     if IS_SMS_ENABLED:
-        for number in SMS_RECIPIENTS:
+        for number in get_recipients():
             SMS.objects.create(number=number, message=message)
 
 
