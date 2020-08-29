d80963aabb2c89be2a69dd6b57e7cba7c5e4d7e6
pmaigutyak <pmaigutyak@gmail.com>
2020-08-29 09:14:32 +0300

    Add settings to init.

--- turbosms/__init__.py
+++ turbosms/__init__.py
@@ -1,11 +1,7 @@
 
-from turbosms.lib import (
-    send_sms_from_template, send_sms, get_default_sms_recipients)
 from turbosms.settings import TurboSMSSettings
 
 
 __all__ = [
-    'send_sms',
-    'send_sms_from_template',
-    'get_default_sms_recipients'
+    'TurboSMSSettings'
 ]
